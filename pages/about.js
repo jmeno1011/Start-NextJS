b3ee1e7876b27164f7331d7de232ce95ff87e4fa
jmeno1011 <jwhltn8282@gmail.com>
2022-05-17 00:41:53 +0900

    next/router 잠깐 맛봄

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,12 @@
+import NavBar from "../components/Navbar";
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <NavBar />
+      <h2>About</h2>
+    </div>
+  );
 }
 
 export default About;
